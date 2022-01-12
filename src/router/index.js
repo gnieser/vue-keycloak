d7901c8f545381452b41149f66d32b96c4ba6b37
gnieser <guillaume.nieser@ibbl.lu>
2022-01-12 09:41:53 +0100

    Switching to Hash mode with logging

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, START_LOCATION } from 'vue-router'
 import routes from "./routes";
 
 // FIXME using createWebHashHistory leads to 404 after login redirection
-const createHistory = createWebHistory // Works as expected
-// const createHistory = createWebHashHistory
+// const createHistory = createWebHistory // Works as expected
+const createHistory = createWebHashHistory
 
 // const createHistory = process.env.SERVER
 //   ? createMemoryHistory
@@ -12,8 +12,16 @@ const createHistory = createWebHistory // Works as expected
 const Router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
   routes,
-  history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
+  history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE),
+})
 
+// Console logging of to and from
+Router.beforeEach((to, from) => {
+  console.log("Router to:", to)
+  console.log("Router from:", from)
+  if (from === START_LOCATION) {
+    // initial navigation
+  }
 })
 
 export default Router
